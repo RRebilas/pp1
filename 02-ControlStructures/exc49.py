48dcb07876f392bd96d5a766aa5d0575ee339fdd
Rafal Rebilas <rrafal7@gmail.com>
2019-10-27 17:43:13 +0100

    better version

--- 02-ControlStructures/exc49.py
+++ 02-ControlStructures/exc49.py
@@ -4,21 +4,19 @@ day = 1
 for i in days:
     print(f"| {i} |", end="")
 print()
+row = 5 if nr_dnia_Tygodnia + 30 > 35 else 4
 for i in range(0, 7):
     if i >= nr_dnia_Tygodnia:
-        print(f"|  {day} |", end="")
+        print("|{0:^4}|".format(day), end="")
         day += 1
     else:
         print("|    |", end="")
 print()
-i = day
-while i <= 30:
-    if 31 > i >= 10:
-        print(f"| {i} |", end="")
-    elif i < 10:
-        print(f"|  {i} |", end="")
-    elif i >= 31:
-        print("|    |", end="")
-    if (i + nr_dnia_Tygodnia) % 7 == 0:
-        print()
-    i += 1
+for i in range(row):
+    for j in range(7):
+        if day > 30:
+            print("|    |", end="")
+        else:
+            print("|{0:^4}|".format(day), end="")
+        day += 1
+    print("")
