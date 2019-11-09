d54639803de302c61471a343dd9ec2a257fe34b4
RRebilas <rrafal7@gmail.com>
2019-11-09 15:33:31 +0100

    another task

--- 04-Subroutines/exc36.py
+++ 04-Subroutines/exc36.py
@@ -1,7 +1,14 @@
-def suma(tablica, i):
-    if i > 0:
-        return tablica[i] + tablica[i - 1]
-
 tab = [7, 5, [3, 6, [2]], 7, [1, [2, 3, [4]], 9, 2], 4]
-tab1 = [1, 2, 3, 4, 5, 6]
-print(suma(tab1, len(tab1) - 1))
+
+
+def suma(tablica):
+    suma_tablicy = 0
+    for i in tablica:
+        if isinstance(i, int):
+            suma_tablicy += i
+        else:
+            suma_tablicy += suma(i)
+    return suma_tablicy
+
+
+print(suma(tab))
